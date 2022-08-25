7376cfc8c75c20059b532c0a31636428c7d856a4
mariapaulav <mariapaulaviviz@gmail.com>
2022-08-25 12:34:42 -0500

    toogle faq finished

---
+++ TOOGLE/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'ARE YOUR FINISHED PRODUCTS TESTED ON ANIMALS?',
+    info:
+      'No. We do not conduct any animal testing on finished goods internally, nor do we conduct any animal testing on finished products either via our suppliers or via a third-party facility.',
+  },
+  {
+    id: 2,
+    title: 'DO YOUR SUPPLIERS USE INGREDIENTS THAT WERE TESTED ON ANIMALS?',
+    info:
+      'No, we do not conduct any animal testing on ingredients internally, nor do we conduct any animal testing on ingredients either via our suppliers or via a third-party facility.',
+  },
+  {
+    id: 3,
+    title: 'DO YOU OBTAIN ASSURANCE FROM YOUR SUPPLIERS THAT THESE INGREDIENTS ARE NOT TESTED (OR HOW DO YOU VERIFY?) ',
+    info:
+      'We ask all vendors to provide vegan and cruelty-free statements to ensure animal testing is not done on any finished products or ingredients.',
+  },
+  {
+    id: 4,
+    title: 'DO YOU HAVE LEAPING BUNNY, CCF OR PETA CERTIFICATIONS? ',
+    info:
+      'We are a Leaping Bunny-certified brand. Additionally, all of our vegan products are PETA-certified.  ',
+  },
+  {
+    id: 5,
+    title: 'DO YOU USE ANIMAL HAIR IN YOUR BRUSHES?',
+    info:
+      'All Makeup by Mario brushes are cruelty-free—made with a custom blend of synthetic and vegan fibers and 100% FSC-certified Birch Wood handles.',
+  },
+]
+export default questions
